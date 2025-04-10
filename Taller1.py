b886ffd8d9bee5fdd8caa2f4809bb4a311cc2b22
SantiagoAjala0204 <santiago.ajala@yachaytech.edu.ec>
2025-04-10 00:43:51 -0500

    Update_Comment_Code

--- Taller1.py
+++ Taller1.py
@@ -2,25 +2,30 @@ import pandas as pd
 import mysql.connector
 from sqlalchemy import create_engine
 
-# Leer el archivo Excel
+#PARTE 1
+#1.- Leer el archivo Excel 
 df = pd.read_excel('C:/Users/USUARIO/Desktop/Maestría_Ciencia_Datos/2.- Fundamentos Ciencia de Datos/Online Retail.xlsx')
 print(df.head())
 
 #########################################################################
-# Realizar la limpieza de los datos
+
+# 2.- Limpieza de los datos
+
 # Imprimir el tipo de datos de las columnas
-print("Tipo de datos antes:", df.dtypes)
+print("\n Tipo de datos antes:", df.dtypes)
 
 # Cambiar el tipo de dato a las columnas
-df["InvoiceNo"] = df["InvoiceNo"].astype(str)
-df["StockCode"] = df["StockCode"].astype(str)
-df["Description"] = df["Description"].astype(str)
-df["Country"] = df["Country"].astype(str)
-df["CustomerID"] = df["CustomerID"].astype(str)
+df["InvoiceNo"]=df["InvoiceNo"].astype('string')
+df["StockCode"]=df["StockCode"].astype('string')
+df["Description"]=df["Description"].astype('string')
+df["Country"]=df["Country"].astype('string')
+df["CustomerID"]=df["CustomerID"].astype('string')
 
-print("Tipo de datos después:", df.dtypes)
+
+print("\n Tipo de datos después:", df.dtypes)
 
 # Revisar valores faltantes en las columnas
+
 def null_values(df):
     for col in df.columns:
         if df[col].isnull().any():
@@ -28,19 +33,19 @@ def null_values(df):
         else:
             print(f"La columna {col} no tiene valores nulos")
 
-print("Columnas antes de eliminar valores faltantes")
+print("\n Columnas antes de eliminar valores faltantes")
 null_values(df)
 
 # Eliminar valores nulos en la columna CustomerID
 df = df.dropna(subset=['CustomerID'])
-print("Columnas después de eliminar valores faltantes")
+print("\n Columnas después de eliminar valores faltantes")
 null_values(df)
 
 # Filtrar Quantity > 0 o UnitPrice > 0
 df = df[(df['Quantity'] > 0) & (df['UnitPrice'] > 0)]
 
 # Imprimir el tamaño del DataFrame
-print(f"El tamaño del df es {df.shape}")
+print(f"\n El tamaño del df es {df.shape}")
 
 # Convertir la columna InvoiceDate a tipo datetime
 df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
@@ -50,6 +55,7 @@ count = df['InvoiceNo'].str.startswith('C', na=False).sum()
 print(f"Hay {count} registros en 'InvoiceNo' que empiezan con 'C'.")
 
 ###########################################################
+# 3.- Subir los datos limpios a una base de datos SQLite
 ## Conectar a la base de datos MySQL
 # Parámetros de conexión
 usuario = 'root'
@@ -66,7 +72,7 @@ df.to_sql(name='online_retails', con=conexion_load, if_exists='replace', index=F
 
 print("✅ ¡Datos subidos exitosamente a la base de datos!")
 
-
+#PARTE2 
 #Leer la tabla desde MySQL y guardarla en un DataFrame
 # Crear la conexión usando mysql-connector
 conexion = mysql.connector.connect(
